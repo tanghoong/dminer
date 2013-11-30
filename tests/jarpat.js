2d7952c3c5f3bcc846f9c875892c2babee2b932a
Gabor Sar <gabor.sar@artoon.hu>
2013-11-30 23:09:44 +0000

    updated to use mocha testing framework

--- tests/jarpat.js
+++ tests/jarpat.js
@@ -1,16 +1,33 @@
 /*jslint node: true */
 
-require('../lib/global.js');
+var assert, jarpat, dataset, expected, clusters;
 
-// test the Jarvis-Patrick clustering method (cli)
-var dataset, jarpat, clusters;
+// I need a Mocha!
+assert = require('assert');
 
-// get input dataset
-dataset = require('./dataset.json');
-
-// get clusters
+// get Jarvis-Patrick clusterig
 jarpat = require('../lib/jarpat.js');
-clusters = jarpat.clusters(dataset, 2, 1);
 
-// print clusters
-console.log(clusters);
+// get dataset
+dataset = [
+    [111, 112, 113], [111, 112, 114], [111, 115, 113],
+    [221, 222, 223], [221, 222, 224], [221, 225, 223],
+    [331, 332, 333], [331, 332, 334], [331, 335, 333],
+    [441, 442, 443]
+];
+
+// get expected clusters
+expected = [
+    [[111, 112, 113], [111, 112, 114], [111, 115, 113]],
+    [[221, 222, 223], [221, 222, 224], [221, 225, 223]],
+    [[331, 332, 333], [331, 332, 334], [331, 335, 333]],
+    [[441, 442, 443]]
+];
+
+// test Jarvis-Patrick clustering
+describe('jarpat', function () {
+    it('should return the expected clusters', function ()  {
+        clusters = jarpat.clusters(dataset, 2, 1);
+        assert.deepEqual(expected, clusters);
+    });
+});
