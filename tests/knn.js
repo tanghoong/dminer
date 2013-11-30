77e39aceb7be52b82755bb0f79b649544ca154ac
Gabor Sar <gabor.sar@artoon.hu>
2013-11-30 22:51:55 +0000

    updated to use mocha testing framework

--- tests/knn.js
+++ tests/knn.js
@@ -1,11 +1,11 @@
 /*jslint node: true */
 
-require('../lib/global.js');
+var assert, knn;
 
-// test the Jarvis-Patrick clustering method (cli)
-var dataset, knn, clusters, labels;
+// I need a Mocha!
+assert = require('assert');
 
-// get classifier
+// get KNN classifier
 knn = require('../lib/knn.js');
 
 // training process
@@ -19,18 +19,26 @@ knn.addDocument(1, [11, 12, 13])
    .addDocument(3, [31, 32, 34])
    .addDocument(3, [31, 34, 35]);
 
-// test perfect match (expected: [1])
-labels = knn.classify([11, 12, 16], 5);
-console.log(labels, [1]);
+// test KNN classifier
+describe('KNN', function () {
 
-// test two matches (expected: [1, 2])
-labels = knn.classify([11, 21, 36], 5);
-console.log(labels, [1, 2]);
+    it('should find 0 matches', function () {
+        var labels = knn.classify([41, 42, 43], 5);
+        assert.equal(0, labels.length);
+    });
 
-// test three matches (expected: [1, 2, 3])
-labels = knn.classify([11, 21, 31], 9);
-console.log(labels, [1, 2, 3]);
+    it('should find 1 match', function () {
+        var labels = knn.classify([11, 12, 16], 5);
+        assert.deepEqual(labels, [1]);
+    });
 
-// test no matches (expected: [])
-labels = knn.classify([41, 42, 43], 5);
-console.log(labels, []);
+    it('should find 2 matches', function () {
+        var labels = knn.classify([11, 21, 36], 5);
+        assert.deepEqual(labels, [1, 2]);
+    });
+
+    it('should find 3 matches', function () {
+        var labels = knn.classify([11, 21, 31], 9);
+        assert.deepEqual(labels, [1, 2, 3]);
+    });
+});
